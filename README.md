651b4720af5fa13b1966faf66c3bc1d3a092b498
Roshni Seth <roshniseth92@gmail.com>
2025-10-22 23:36:27 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# STS_Api
+title: Semantic Textual Similarity API
+emoji: 📝
+colorFrom: blue
+colorTo: green
+sdk: gradio
+sdk_version: 5.49.1
+app_file: api_app.py
+pinned: false
--- api_app.py
+++ api_app.py
@@ -2,14 +2,13 @@
 import gradio as gr
 from sentence_transformers import SentenceTransformer, util
 
-# Load model once
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 def predict(text1, text2):
     emb1 = model.encode(text1, convert_to_tensor=True)
     emb2 = model.encode(text2, convert_to_tensor=True)
-    score = util.cos_sim(emb1, emb2).item()
-    normalized_score = (score + 1)/2
+    similarity = util.cos_sim(emb1, emb2).item()
+    normalized_score = (similarity + 1) / 2
     return {"similarity score": round(normalized_score, 4)}
 
 iface = gr.Interface(
